f1645160786f85e98a2fdab205a5de519c088fec
Jefferson <guillot0777@gmail.com>
2025-04-30 18:27:07 -0500

    alert de juego no iniciado

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -63,7 +63,9 @@ const miModulo = (() =>{
     const perdirCarta = () =>{
 
         if (deck.length === 0){
-            throw 'no hay cartas en el deck';   
+            alert("No hay cartas en el deck, debe iniciar juego")  
+            throw 'no hay cartas en el deck';
+             
         }
         return deck.pop();
     }
